5dfb72231ff7ccde35d33d992bfd589ce7bbb367
Laurent Bercot <ska-skaware@skarnet.org>
2023-02-06 17:13:47 +0000

    Update s6 dep; simplify servicedir block/unblock
    Signed-off-by: Laurent Bercot <ska@appnovation.com>

--- src/libs6rc/s6rc_servicedir_block.c
+++ src/libs6rc/s6rc_servicedir_block.c
@@ -1,28 +1,13 @@
 /* ISC license. */
 
-#include <string.h>
-#include <unistd.h>
-
-#include <skalibs/posixplz.h>
-#include <skalibs/djbunix.h>
-
 #include <s6/supervise.h>
 
 #include <s6-rc/s6rc-servicedir.h>
 
 int s6rc_servicedir_block (char const *dir)
 {
-  size_t dirlen = strlen(dir) ;
   s6_svstatus_t status ;
-  char fn[dirlen + 6] ;
   if (!s6_svstatus_read(dir, &status)) return -1 ;
-  memcpy(fn, dir, dirlen) ;
-  memcpy(fn + dirlen, "/down", 6) ;
-  if (!touch(fn)) return -1 ;
-  if (s6_svc_writectl(dir, S6_SUPERVISE_CTLDIR, "O", 1) < 0)
-  {
-    unlink_void(fn) ;
-    return -1 ;
-  }
+  if (s6_svc_writectl(dir, S6_SUPERVISE_CTLDIR, "Q", 1) < 0) return -1 ;
   return status.flagwantup ;
 }
--- src/libs6rc/s6rc_servicedir_unblock.c
+++ src/libs6rc/s6rc_servicedir_unblock.c
@@ -1,23 +1,11 @@
 /* ISC license. */
 
-#include <string.h>
-#include <errno.h>
-#include <unistd.h>
-
 #include <s6/supervise.h>
 
 #include <s6-rc/s6rc-servicedir.h>
 
 int s6rc_servicedir_unblock (char const *dir, int h)
 {
-  if (h)
-  {
-    size_t dirlen = strlen(dir) ;
-    char fn[dirlen + 6] ;
-    memcpy(fn, dir, dirlen) ;
-    memcpy(fn + dirlen, "/down", 6) ;
-    if (unlink(fn) < 0 && errno != ENOENT) return -1 ;
-    if (s6_svc_writectl(dir, S6_SUPERVISE_CTLDIR, "u", 1) < 0) return -1 ;
-  }
+  if (h && s6_svc_writectl(dir, S6_SUPERVISE_CTLDIR, "U", 1) < 0) return -1 ;
   return 0 ;
 }
